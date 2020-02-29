8941ff69c252a8d9b59bd6c2294db713274e6e04
Artem Kamyshev <oemrus@yandex.ru>
2020-03-01 00:22:31 +0300

    Use run_cpt action instead of locally added code

--- build.py
+++ build.py
@@ -1,5 +1,12 @@
+from os import environ
 from cpt.packager import ConanMultiPackager
 
+
+print('---CONAN_ env vars')
+for v in environ:
+    if v.startswith('CONAN_') and v != 'CONAN_PASSWORD':
+      print(v)
+print('---CONAN_ env vars')
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
