43338b83ad012d84a2d85bea25c4fbafabcf24e5
Artem Kamyshev <oemrus@yandex.ru>
2020-03-02 22:07:22 +0300

    Remove debug logs from build.py

--- build.py
+++ build.py
@@ -2,12 +2,6 @@ from os import environ
 from cpt.packager import ConanMultiPackager
 
 
-print('---CONAN_ env vars')
-for v in environ:
-    if v.startswith('CONAN_') and v != 'CONAN_PASSWORD':
-      print(v)
-print('---CONAN_ env vars')
-
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=False)
